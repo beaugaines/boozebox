148655bbcb370811fa8c88c0cbbde7655a328550
Charlie Gaines <beaugaines@gmail.com>
2014-03-03 15:17:21 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Boozebox::Application.config.secret_key_base = '5b9deeced39a84c384107b48c75c4414015cf1d72d545c61c836b8abba36e3cb9e424f384e157736304726376df6bf2b65f6394e652b1b2f270426d6d8d427d2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Boozebox::Application.config.session_store :cookie_store, key: '_boozebox_session'
